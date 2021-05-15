9ffd4fb110614eafecad3e5b692aec5233c1f7c6
phdgamer <pedro380dalpa@gmail.com>
2021-05-15 16:52:42 -0300

    Create a service to create a category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CreateCategoryService } from '../services/CreateCategoryService';
 
 const categoriesRoutes = Router();
 
@@ -8,13 +9,10 @@ const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
-  const categoryAlreadyExist = categoriesRepository.findByName(name);
 
-  if (categoryAlreadyExist) {
-    return response.status(400).json({ error: 'Category already exist!' });
-  }
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
 
-  categoriesRepository.create({ name, description });
+  createCategoryService.execute({ name, description });
 
   return response.status(201).send();
 });
