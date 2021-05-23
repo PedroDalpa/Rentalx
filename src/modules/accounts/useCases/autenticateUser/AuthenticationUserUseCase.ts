afe6a34c3712285b699967db434cfd97997f69f8
phdgamer <pedro380dalpa@gmail.com>
2021-05-23 16:13:18 -0300

    Create authenticate method

---
+++ src/modules/accounts/useCases/autenticateUser/AuthenticationUserUseCase.ts
@@ -0,0 +1,54 @@
+import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+
+import { IUserRepository } from '../../repositories/IUsersRepository';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticationUserUseCase {
+  constructor(
+    @inject('UserRepository')
+    private userRepository: IUserRepository
+  ) {}
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.userRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error('Email or password incorrect!');
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error('Email or password incorrect!');
+    }
+
+    const token = sign({}, 'secret', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+      token,
+    };
+  }
+}
+
+export { AuthenticationUserUseCase };
---
+++ src/routes/authenticate.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+
+import { AuthenticationUserController } from '../modules/accounts/useCases/autenticateUser/AuthenticationUserController';
+
+const authenticateRoutes = Router();
+
+const authenticationUserController = new AuthenticationUserController();
+
+authenticateRoutes.post('/sessions', authenticationUserController.handle);
+
+export { authenticateRoutes };
